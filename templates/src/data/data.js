956a6531f1fa72df1e522e4efb0e76e68b7d8c72
Heorhi Davydau <heorhid@gmail.com>
2024-03-01 19:19:33 +0100

    add products.html && data.js

---
+++ templates/src/data/data.js
@@ -0,0 +1,134 @@
+export const products = [
+  {
+    id: 1,
+    name: "Ultramatte Teardown",
+    price: 24.95,
+    image: "public/ZIdo5kxfdwrI_XBt.webp",
+    label: "Designed by Zack. Built by Robots.",
+    description:
+      "Made in collaboration with JerryRigEverything, our Teardown skin uses an optically clear gloss finish to ensure you see every dollar you wasted.",
+    model: "iphone-15-pro-max",
+    count: 100,
+  },
+  {
+    id: 2,
+    name: "TEARDOWN",
+    price: 31.95,
+    image: "public/b1kBnufl0WKE2rNO.webp",
+    label: "Designed by Zack. Built by Robots.",
+    description:
+      "Made in collaboration with JerryRigEverything, our Teardown skin uses an optically clear gloss finish to ensure you see every dollar you wasted.",
+    model: "macbook-pro-16-2023-m3",
+    count: 100,
+  },
+  {
+    id: 3,
+    name: "Real Brown Leather",
+    price: 49.95,
+    image: "public/GM0g_CTK6q_BkzO7.webp",
+    label: "Full-grain Brown Leather. Cows died for this.",
+    description:
+      "100% authentic, uncorrected, full-grain, vegetable-tanned leather - no textured stamping or chemical treatment. Over time, scuffs, natural oils & daily wear-and-tear combine to form a timeless patina. Unlike you, this leather will only get better with age. Thanks, Bessie.",
+    model: "ipad-pro-129-2022-gen-6",
+    count: 100,
+  },
+  {
+    id: 4,
+    name: "Solstice",
+    price: 24.95,
+    image: "public/L25o81z9ov-tGZlK.webp",
+    label: "Hot and cold, but mostly cold.",
+    description:
+      "Made in collaboration with Ali Abdaal. The \"hot and cold\" imagery reflects life's up and downs. Much like your life, it's mostly cold.",
+    model: "iphone-15-pro-max",
+    count: 54,
+  },
+  {
+    id: 5,
+    name: "ACID",
+    price: 24.95,
+    image: "public/kpqiMaWULDh-CGRv.webp",
+    label: "Toxic electric tape.",
+    description:
+      "Like any self-respecting business, we use temps to taste-test our new product launches. Apparently, our staffing agency had a problem with this - they said the temps kept coming back with intense stomach pain. Say no more, staffing agency: next time, they won't come back.",
+    model: "iphone-15-pro-max",
+    count: 189,
+  },
+  {
+    id: 6,
+    name: "TMNP",
+    price: 24.95,
+    image: "public/MJgNvWrjrUoEZohF.webp",
+    label: "Pixels in a half-shell.",
+    description:
+      "You wake up. It's 1993. A Saturday. You don't have to go to school. You sit down in front of the TV with a bowl of Cap'n Crunch. The Turtles are fighting some aliens today. You're going to Jeff's house this afternoon to play Mortal Kombat. It's the last time you'll ever be happy.",
+    model: "pixel-8-pro",
+    count: 13,
+  },
+  {
+    id: 7,
+    name: "GREEN CAMO",
+    price: 24.95,
+    image: "public/n32FcnC1wxxBhon1.webp",
+    label: "Army tested, civilian approved.",
+    description:
+      "You've never seen combat. In fact, you'd survive about five seconds in an actual firefight. Luckily, Green Camo will let you continue cosplaying in the safety of your mom's backyard.",
+    model: "ipad-pro-129-2022-gen-6",
+    count: 90,
+  },
+  {
+    id: 8,
+    name: "BLACK CARBON",
+    price: 16.95,
+    image: "public/YlOehQim0b1spTOs.webp",
+    label: "The finest Lamborghini cosplay.",
+    description:
+      "Kevlar vests. Elite golf clubs. Custom car hoods. You can't afford any of these things. Thanks to our Black Carbon material, you can at least pretend.",
+    model: "iphone-15-pro-max",
+    count: 100,
+  },
+  {
+    id: 9,
+    name: "DEADQUARTERS",
+    price: 31.95,
+    image: "public/XwYmRvcEJpe6FeMN.webp",
+    label: "Every body needs a grave to call home.",
+    description:
+      "One of Robot City's innumerable districts, the Deadquarters fulfill two purposes. As the city's administrative nerve center, this is the place where dreams go to die. Owing to the city's progressive stance on euthanasia, it's also where people go to die - assuming they live long enough to reach the front of the line.",
+    model: "macbook-pro-16-2023-m3",
+    count: 100,
+  },
+  {
+    id: 10,
+    name: "X-RAY (LIGHT)",
+    price: 24.95,
+    image: "public/Lezk5_yDufiT5zzE.webp",
+    label: "Made with 100% real X-Rays.",
+    description:
+      "This isn’t your doctor’s X-Ray. Each bespoke scan was performed in a metrology lab with one-of-a-kind imaging hardware, originally developed for space exploration. It's literal rocket science. Considering you're not a rocket scientist, it seemed unfair to expect you to choose between light and dark mode. Instead, you can buy one and we’ll give you the other for free.",
+    model: "galaxy-s24-ultra",
+    count: 87,
+  },
+  {
+    id: 11,
+    name: "X-RAY (LIGHT)",
+    price: 31.95,
+    image: "public/04XBU9GXV2H-Y5c0.webp",
+    label: "Made with 100% real X-Rays.",
+    description:
+      "This isn’t your doctor’s X-Ray. Each bespoke scan was performed in a metrology lab with one-of-a-kind imaging hardware, originally developed for space exploration. It's literal rocket science. Considering you're not a rocket scientist, it seemed unfair to expect you to choose between light and dark mode. Instead, you can buy one and we’ll give you the other for free.",
+    model: "macbook-pro-16-2023-m3",
+    count: 230,
+  },
+  {
+    id: 12,
+    name: "X-RAY (LIGHT)",
+    price: 24.95,
+    image: "public/_DgljNPkHEJPUm8z.webp",
+    label: "Made with 100% real X-Rays.",
+    description:
+      "This isn’t your doctor’s X-Ray. Each bespoke scan was performed in a metrology lab with one-of-a-kind imaging hardware, originally developed for space exploration. It's literal rocket science. Considering you're not a rocket scientist, it seemed unfair to expect you to choose between light and dark mode. Instead, you can buy one and we’ll give you the other for free.",
+    model: "iphone-15-pro-max",
+    count: 33,
+  },
+];
---
+++ templates/src/js/products.js
@@ -0,0 +1,259 @@
+import { products } from "../data/data.js";
+
+const filterProducts = (products, filter) => {
+  if (!products || !products.length || !filter) {
+    return [];
+  }
+
+  let filteredProducts = [...products];
+
+  // Filter by min and max price
+  if (filter.price && filter.price.min !== undefined && filter.price.max !== undefined) {
+    filteredProducts = filteredProducts.filter(product =>
+      product.price >= filter.price.min && product.price <= filter.price.max
+    );
+  }
+
+  // Filter by model
+  if (filter.model && filter.model.length > 0) {
+    filteredProducts = filteredProducts.filter(product =>
+      filter.model.includes(product.model)
+    );
+  }
+
+  // Sort by price
+  if (filter.sort && filter.sort.orderByPrice) {
+     switch (filter.sort.orderByPrice) {
+       case "asc":
+         filteredProducts.sort((a, b) => a.price - b.price);
+         break;
+       case "desc":
+         filteredProducts.sort((a, b) => b.price - a.price);
+         break;
+     }
+   }
+
+  // Sort by model
+  if (filter.sort && filter.sort.orderByModel) {
+    if (filter.sort.orderByModel === "asc") {
+      filteredProducts.sort((a, b) => a.model.localeCompare(b.model));
+    }
+  }
+
+  // Search by name
+  if (filter.search && filter.search.query !== "") {
+    filteredProducts = filteredProducts.filter(product =>
+      product.name.toLowerCase().includes(filter.search.query.toLowerCase())
+    );
+  }
+
+  return filteredProducts;
+};
+
+const countFilters = (filter) => {
+  let count = 0;
+
+  if (filter.model && filter.model.length > 0) {
+    count += filter.model.length;
+  }
+
+  if (filter.price && (filter.price.min !== undefined || filter.price.max !== undefined)) {
+    count++;
+  }
+
+  if (filter.search && filter.search.query !== "") {
+    count++;
+  }
+
+  return count || "";
+};
+
+const showProducts = (products, filter) => {
+  const filteredProducts = filterProducts(products, filter);
+  renderMenu(filteredProducts, countFilters(filter));
+};
+
+const renderMenu = (products, filtersNumber) => {
+  const filterTextElement = document.getElementById("filters-text");
+  filterTextElement.innerText = `${filtersNumber} Filter${filtersNumber !== 1 ? 's' : ''}`;
+
+  const productsContainerElement = document.getElementById("products-container");
+  productsContainerElement.innerHTML = "";
+
+  if (products.length === 0) {
+    productsContainerElement.innerHTML = createEmptyProductsHTML();
+  }
+  else {
+    products.forEach(product => {
+      const productElement = createProductsHTML(product);
+      productsContainerElement.innerHTML += productElement;
+    });
+  }
+};
+
+const createEmptyProductsHTML = () => `
+  <div class="h-96 w-full flex items-center justify-center col-span-full flex-col">
+      <h2 class="text-lg font-medium text-gray-900">Sorry, no products found</h2>
+      <p class="mt-1 text-sm text-gray-500">Please clear the filters to see all products</p>
+  </div>
+`;
+
+const createProductsHTML = (product) => `
+  <div class="group relative">
+      <div class="bg-w h-56 w-full overflow-hidden rounded-md group-hover:opacity-75 lg:h-72 xl:h-80 p-4">
+          <img
+              width=180
+              height=320
+              src="${product.image}"
+              alt="${product.name}"
+              class="object-fit h-full w-full object-contain object-center"
+          />
+      </div>
+      <h3 class="mt-4 text-sm text-gray-700">
+          <a href="product.html">
+              <span class="absolute inset-0"></span>
+              ${product.name}
+          </a>
+      </h3>
+      <p class="mt-1 text-sm text-gray-500">${product.label}</p>
+      <p class="mt-1 text-sm font-medium text-gray-900">$${product.price}</p>
+  </div>
+`;
+
+const createProxiedProperty = (property, updateUI) => {
+  return new Proxy(property, {
+    set: (target, property, value) => {
+      target[property] = value;
+      updateUI();
+      return true;
+    },
+  });
+};
+
+// Filter state generator, it returns a proxy object that updates the UI when the state changes
+const createFilterState = (products) => {
+  const filter = {
+    // Filter by model
+    model: createProxiedProperty([],
+      () => showProducts(products, filter)),
+    // Filter by price
+    price: createProxiedProperty({
+      min: undefined, max: undefined
+    }, () => showProducts(products, filter)),
+    // Sorting
+    sort: createProxiedProperty({
+      orderByPrice: undefined,
+      orderByModel: undefined,
+    }, () => showProducts(products, filter)),
+    // Search
+    search: createProxiedProperty({
+      query: ""
+    }, () => showProducts(products, filter))
+  };
+  showProducts(products, filter);
+  return filter;
+};
+
+const renderFilters = () => {
+  const filter = createFilterState(products);
+
+  let filterMenuState = false;
+
+  const filtersButtonElement = document.getElementById("filters-button");
+  const filterMenuElement = document.getElementById("filters-menu");
+
+  filtersButtonElement.addEventListener("click", () => {
+    filterMenuState = !filterMenuState;
+    filterMenuElement.style.display = filterMenuState ? "block" : "none";
+  });
+
+  const searchInputElement = document.getElementById("search");
+  searchInputElement.addEventListener("input", (event) => {
+    filter.search.query = event.target.value;
+    console.log(filter.search.query);
+  });
+
+  const filterFormElemt = document.getElementById("filter-form");
+
+  let menuState = false
+  const menuButtonElement = document.getElementById("menu-button");
+  const menuElement = document.getElementById("menu");
+
+  menuButtonElement.addEventListener("click", () => {
+    menuState = !menuState;
+    menuElement.style.display = menuState ? "block" : "none";
+  });
+
+  const menuItemsElements = [
+    document.getElementById("menu-item-0"),
+    document.getElementById("menu-item-1"),
+    document.getElementById("menu-item-2"),
+  ];
+
+  const menuItemState = { active: undefined };
+
+  menuItemsElements.forEach((menuItemElement) => {
+    menuItemElement.addEventListener("click", (event) => {
+      if (menuItemState.active === event.target.id) {
+        menuItemElement.className = "text-gray-500 block px-4 py-2 text-sm hover:bg-indigo-100";
+        menuItemState.active = undefined;
+        filter.sort.orderByPrice = undefined;
+        filter.sort.orderByModel = undefined;
+      }
+      else {
+
+        if (menuItemState.active !== undefined) {
+          menuItemsElements.forEach((menuItemElement) => {
+            menuItemElement.className = "text-gray-500 block px-4 py-2 text-sm hover:bg-indigo-100";
+          });
+        }
+
+        menuItemState.active = event.target.id;
+        menuItemElement.className = "bg-gray-100 font-medium text-gray-900 block px-4 py-2 text-sm hover:bg-gray-200";
+        switch (event.target.innerText) {
+          case "Lowest Price":
+            filter.sort.orderByPrice = "asc";
+            break;
+          case "Highest Price":
+            filter.sort.orderByPrice = "desc";
+            break;
+          case "Model":
+            filter.sort.orderByModel = "asc";
+            break;
+        }
+      }
+
+      menuState = false;
+      menuElement.style.display = "none";
+    });
+  });
+
+  const priceFilterElement = document.getElementById("price-filter");
+  priceFilterElement.addEventListener("change", (event) => {
+    [filter.price.min, filter.price.max] = event.target.value.split("-");
+  });
+
+  const modelFilterElement = document.getElementById("model-filter");
+  modelFilterElement.addEventListener("change", (event) => {
+    event.target.checked
+      ? filter.model.push(event.target.value)
+      : filter.model.splice(filter.model.indexOf(event.target.value), 1);
+  });
+
+  const clearFilterElement = document.getElementById("clear-filters");
+  clearFilterElement.addEventListener("click", () => {
+    filterMenuElement.style.display = "none";
+    filter.price.min = undefined;
+    filter.price.max = undefined;
+    filter.model.length = 0;
+    filter.sort.orderByPrice = undefined;
+    filter.sort.orderByModel = undefined;
+    filter.search.query = "";
+    searchInputElement.value = "";
+    filterFormElemt.reset();
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderFilters();
+});
