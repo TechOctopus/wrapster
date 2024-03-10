7e4798a1969137246491628e4e0bc81f153a9a58
Heorhi Davydau <heorhid@gmail.com>
2024-03-10 12:31:41 +0100

    redesign product and add 2 img

--- templates/src/data/data.js
+++ templates/src/data/data.js
@@ -4,7 +4,10 @@ export const products = [
     name: "Ultramatte Teardown",
     collection: "ultramatte_teardown",
     price: 24.95,
-    image: "public/ZIdo5kxfdwrI_XBt.webp",
+    image: [
+      "public/ZIdo5kxfdwrI_XBt.webp",
+      "public/Jfn6jRvlwKqcXHMm.webp"
+    ],
     label: "Designed by Zack. Built by Robots.",
     description:
       "Made in collaboration with JerryRigEverything, our Teardown skin uses an optically clear gloss finish to ensure you see every dollar you wasted.",
@@ -16,7 +19,10 @@ export const products = [
     name: "TEARDOWN",
     collection: "teardown",
     price: 31.95,
-    image: "public/b1kBnufl0WKE2rNO.webp",
+    image: [
+      "public/b1kBnufl0WKE2rNO.webp",
+      "public/Jfn6jRvlwKqcXHMm.webp"
+    ],
     label: "Designed by Zack. Built by Robots.",
     description:
       "Made in collaboration with JerryRigEverything, our Teardown skin uses an optically clear gloss finish to ensure you see every dollar you wasted.",
@@ -28,7 +34,10 @@ export const products = [
     name: "Real Brown Leather",
     collection: "real_brown_leather",
     price: 49.95,
-    image: "public/GM0g_CTK6q_BkzO7.webp",
+    image: [
+      "public/GM0g_CTK6q_BkzO7.webp",
+      "public/h9HSzFZEgzHAG5KI.webp"
+    ],
     label: "Full-grain Brown Leather.",
     description:
       "100% authentic, uncorrected, full-grain, vegetable-tanned leather - no textured stamping or chemical treatment. Over time, scuffs, natural oils & daily wear-and-tear combine to form a timeless patina. Unlike you, this leather will only get better with age. Thanks, Bessie.",
@@ -40,7 +49,10 @@ export const products = [
     name: "Solstice",
     collection: "solstice",
     price: 24.95,
-    image: "public/L25o81z9ov-tGZlK.webp",
+    image: [
+      "public/L25o81z9ov-tGZlK.webp",
+      "public/Jfn6jRvlwKqcXHMm.webp"
+    ],
     label: "Hot and cold, but mostly cold.",
     description:
       "Made in collaboration with Ali Abdaal. The \"hot and cold\" imagery reflects life's up and downs. Much like your life, it's mostly cold.",
@@ -52,7 +64,10 @@ export const products = [
     name: "ACID",
     collection: "acid",
     price: 24.95,
-    image: "public/kpqiMaWULDh-CGRv.webp",
+    image: [
+      "public/kpqiMaWULDh-CGRv.webp",
+      "public/Jfn6jRvlwKqcXHMm.webp"
+    ],
     label: "Toxic electric tape.",
     description:
       "Like any self-respecting business, we use temps to taste-test our new product launches. Apparently, our staffing agency had a problem with this - they said the temps kept coming back with intense stomach pain. Say no more, staffing agency: next time, they won't come back.",
@@ -64,7 +79,10 @@ export const products = [
     name: "TMNP",
     collection: "tmnp",
     price: 24.95,
-    image: "public/MJgNvWrjrUoEZohF.webp",
+    image: [
+      "public/MJgNvWrjrUoEZohF.webp",
+      "public/vg2cHAgZIQ6edAo-.webp"
+    ],
     label: "Pixels in a half-shell.",
     description:
       "You wake up. It's 1993. A Saturday. You don't have to go to school. You sit down in front of the TV with a bowl of Cap'n Crunch. The Turtles are fighting some aliens today. You're going to Jeff's house this afternoon to play Mortal Kombat. It's the last time you'll ever be happy.",
@@ -76,7 +94,10 @@ export const products = [
     name: "GREEN CAMO",
     collection: "green_camo",
     price: 24.95,
-    image: "public/n32FcnC1wxxBhon1.webp",
+    image: [
+      "public/n32FcnC1wxxBhon1.webp",
+      "public/h9HSzFZEgzHAG5KI.webp"
+    ],
     label: "Army tested, civilian approved.",
     description:
       "You've never seen combat. In fact, you'd survive about five seconds in an actual firefight. Luckily, Green Camo will let you continue cosplaying in the safety of your mom's backyard.",
@@ -88,7 +109,10 @@ export const products = [
     name: "BLACK CARBON",
     collection: "black_carbon",
     price: 16.95,
-    image: "public/YlOehQim0b1spTOs.webp",
+    image: [
+      "public/YlOehQim0b1spTOs.webp",
+      "public/Jfn6jRvlwKqcXHMm.webp"
+    ],
     label: "The finest Lamborghini cosplay.",
     description:
       "Kevlar vests. Elite golf clubs. Custom car hoods. You can't afford any of these things. Thanks to our Black Carbon material, you can at least pretend.",
@@ -100,7 +124,10 @@ export const products = [
     name: "DEADQUARTERS",
     collection: "deadquarters",
     price: 31.95,
-    image: "public/XwYmRvcEJpe6FeMN.webp",
+    image: [
+      "public/XwYmRvcEJpe6FeMN.webp",
+      "public/XdcylPEgT4YcMeyF.webp"
+    ],
     label: "Every body needs a grave to call home.",
     description:
       "One of Robot City's innumerable districts, the Deadquarters fulfill two purposes. As the city's administrative nerve center, this is the place where dreams go to die. Owing to the city's progressive stance on euthanasia, it's also where people go to die - assuming they live long enough to reach the front of the line.",
@@ -112,7 +139,10 @@ export const products = [
     name: "X-RAY (LIGHT)",
     collection: "x-ray-light",
     price: 24.95,
-    image: "public/Lezk5_yDufiT5zzE.webp",
+    image: [
+      "public/Lezk5_yDufiT5zzE.webp",
+      "public/_Vtco-fva1B0r6xC.webp"
+    ],
     label: "Made with 100% real X-Rays.",
     description:
       "This isn’t your doctor’s X-Ray. Each bespoke scan was performed in a metrology lab with one-of-a-kind imaging hardware, originally developed for space exploration. It's literal rocket science. Considering you're not a rocket scientist, it seemed unfair to expect you to choose between light and dark mode. Instead, you can buy one and we’ll give you the other for free.",
@@ -124,7 +154,10 @@ export const products = [
     name: "X-RAY (LIGHT)",
     collection: "x-ray-light",
     price: 31.95,
-    image: "public/04XBU9GXV2H-Y5c0.webp",
+    image: [
+      "public/04XBU9GXV2H-Y5c0.webp",
+      "public/zglo6sO1SMHl2KwP.webp"
+    ],
     label: "Made with 100% real X-Rays.",
     description:
       "This isn’t your doctor’s X-Ray. Each bespoke scan was performed in a metrology lab with one-of-a-kind imaging hardware, originally developed for space exploration. It's literal rocket science. Considering you're not a rocket scientist, it seemed unfair to expect you to choose between light and dark mode. Instead, you can buy one and we’ll give you the other for free.",
@@ -136,7 +169,10 @@ export const products = [
     name: "X-RAY (LIGHT)",
     collection: "x-ray-light",
     price: 24.95,
-    image: "public/_DgljNPkHEJPUm8z.webp",
+    image: [
+      "public/_DgljNPkHEJPUm8z.webp",
+      "public/Jfn6jRvlwKqcXHMm.webp"
+    ],
     label: "Made with 100% real X-Rays.",
     description:
       "This isn’t your doctor’s X-Ray. Each bespoke scan was performed in a metrology lab with one-of-a-kind imaging hardware, originally developed for space exploration. It's literal rocket science. Considering you're not a rocket scientist, it seemed unfair to expect you to choose between light and dark mode. Instead, you can buy one and we’ll give you the other for free.",
@@ -148,7 +184,10 @@ export const products = [
     name: "Ultramatte Teardown",
     collection: "ultramatte_teardown",
     price: 29.95,
-    image: "public/su8NHDH1k3rau2sy.webp",
+    image: [
+      "public/su8NHDH1k3rau2sy.webp",
+      "public/h9HSzFZEgzHAG5KI.webp"
+    ],
     label: "Designed by Zack. Built by Robots.",
     description:
       "Made in collaboration with JerryRigEverything, our Ultramatte Teardown skin features a premium, fingerprint-resistant texture. You don't deserve it.",
@@ -160,109 +199,149 @@ export const products = [
     name: "Solstice",
     collection: "solstice",
     price: 29.95,
-    image: "public/XXcFBUgg2pp8wlkZ.webp",
+    image: [
+      "public/XXcFBUgg2pp8wlkZ.webp",
+      "public/h9HSzFZEgzHAG5KI.webp"
+    ],
     label: "Hot and cold, but mostly cold.",
     description:
       "Made in collaboration with Ali Abdaal. The \"hot and cold\" imagery reflects life's up and downs. Much like your life, it's mostly cold.",
     model: "ipad-pro-129-2022-gen-6",
+    count: 92
   },
   {
     id: 15,
     name: "DEADQUARTERS",
     collection: "deadquarters",
     price: 29.95,
-    image: "public/0TNPCtktaX7k1C3j.webp",
+    image: [
+      "public/0TNPCtktaX7k1C3j.webp",
+      "public/h9HSzFZEgzHAG5KI.webp"
+    ],
     label: "Every body needs a grave to call home.",
     description:
       "One of Robot City's innumerable districts, the Deadquarters fulfill two purposes. As the city's administrative nerve center, this is the place where dreams go to die. Owing to the city's progressive stance on euthanasia, it's also where people go to die - assuming they live long enough to reach the front of the line.",
     model: "ipad-pro-129-2022-gen-6",
+    count: 90
   },
   {
     id: 16,
     name: "MAGMA",
     collection: "magma",
     price: 24.95,
-    image: "public/7pzknz_YskPf8Jl5.webp",
+    image: [
+      "public/7pzknz_YskPf8Jl5.webp",
+      "public/Jfn6jRvlwKqcXHMm.webp"
+    ],
     label: "Burning a hole in your wallet.",
     description:
       "Made in collaboration with Ali Abdaal. Apparently it's \"impractical\" to mail lava to your house, so this is the next best thing.",
-    model: "iphone-15-pro-max"
+    model: "iphone-15-pro-max",
+    count: 99
   },
   {
     id: 17,
     name: "OBSIDIAN",
     collection: "obsidian",
     price: 59.90,
-    image: "public/TiVfGE8rezCTYN6G.webp",
+    image: [
+      "public/TiVfGE8rezCTYN6G.webp",
+      "public/h9HSzFZEgzHAG5KI.webp"
+    ],
     label: "Gun metal rainbow.",
     description:
       "Ever wanted to make your device look like an environmental disaster? You're in luck: Obsidian manages to simulate a catastrophic oil spill with only a fraction of the toxic fumes. Have you looked at the price of oil lately? You'd be hard-pressed to find a better deal. Checkmate, OPEC.",
-    model: "ipad-pro-129-2022-gen-6"
+    model: "ipad-pro-129-2022-gen-6",
+    count: 56
   },
   {
     id: 18,
     name: "OBSIDIAN",
     collection: "obsidian",
     price: 63.90,
-    image: "public/QRKH2JLdSL5kS06l.webp",
+    image: [
+      "public/QRKH2JLdSL5kS06l.webp",
+      "public/BU9Rzwo2O28PerjM.webp"
+    ],
     label: "Gun metal rainbow.",
     description:
       "Ever wanted to make your device look like an environmental disaster? You're in luck: Obsidian manages to simulate a catastrophic oil spill with only a fraction of the toxic fumes. Have you looked at the price of oil lately? You'd be hard-pressed to find a better deal. Checkmate, OPEC.",
-    model: "macbook-pro-16-2023-m3"
+    model: "macbook-pro-16-2023-m3",
+    count: 34
   },
   {
     id: 19,
     name: "Solstice",
     collection: "solstice",
     price: 24.95,
-    image: "public/RZQD46ELwIoRCsuv.webp",
+    image: [
+      "public/RZQD46ELwIoRCsuv.webp",
+      "public/rQu0Y9yIxy_hftAT.webp"
+    ],
     label: "Hot and cold, but mostly cold.",
     description:
       "Made in collaboration with Ali Abdaal. The \"hot and cold\" imagery reflects life's up and downs. Much like your life, it's mostly cold.",
-    model: "pixel-8-pro"
+    model: "pixel-8-pro",
+    count: 34
   },
   {
     id: 20,
     name: "OBSIDIAN",
     collection: "obsidian",
     price: 24.95,
-    image: "public/pVNMJN6YNa-I-vmy.webp",
+    image: [
+      "public/pVNMJN6YNa-I-vmy.webp",
+      "public/YRXf4rC9YsP328Hp.webp"
+    ],
     label: "Gun metal rainbow.",
     description:
       "Ever wanted to make your device look like an environmental disaster? You're in luck: Obsidian manages to simulate a catastrophic oil spill with only a fraction of the toxic fumes. Have you looked at the price of oil lately? You'd be hard-pressed to find a better deal. Checkmate, OPEC.",
-    model: "galaxy-s24-ultra"
+    model: "galaxy-s24-ultra",
+    count: 33
   },
   {
     id: 21,
     name: "TEARDOWN",
     collection: "teardown",
     price: 24.95,
-    image: "public/zKFdlfbM_g0E83ZN.webp",
+    image: [
+      "public/zKFdlfbM_g0E83ZN.webp",
+      "public/_Vtco-fva1B0r6xC.webp"
+    ],
     label: "Designed by Zack. Built by Robots.",
     description:
       "Made in collaboration with JerryRigEverything, our Teardown skin uses an optically clear gloss finish to ensure you see every dollar you wasted.",
-    model: "galaxy-s24-ultra"
+    model: "galaxy-s24-ultra",
+    count: 94
   },
   {
     id: 22,
     name: "DEADQUARTERS",
     collection: "deadquarters",
     price: 24.95,
-    image: "public/Iwy1Tui3K8Xp5ckb.webp",
+    image: [
+      "public/Iwy1Tui3K8Xp5ckb.webp",
+      "public/_Vtco-fva1B0r6xC.webp"
+    ],
     label: "Every body needs a grave to call home.",
     description:
       "One of Robot City's innumerable districts, the Deadquarters fulfill two purposes. As the city's administrative nerve center, this is the place where dreams go to die. Owing to the city's progressive stance on euthanasia, it's also where people go to die - assuming they live long enough to reach the front of the line.",
-    model: "galaxy-s24-ultra"
+    model: "galaxy-s24-ultra",
+    count: 234
   },
   {
     id: 23,
     name: "MAGMA",
     collection: "magma",
     price: 31.95,
-    image: "public/jSBW2ILEEc_7a3tS.webp",
+    image: [
+      "public/jSBW2ILEEc_7a3tS.webp",
+      "public/QChCogWqwM3CaFhB.webp"
+    ],
     label: "Burning a hole in your wallet.",
     description:
       "Made in collaboration with Ali Abdaal. Apparently it's \"impractical\" to mail lava to your house, so this is the next best thing.",
-    model: "macbook-pro-16-2023-m3"
+    model: "macbook-pro-16-2023-m3",
+    count: 32
   }
 ];
---
+++ templates/src/js/product.js
@@ -0,0 +1,101 @@
+import Carousel from './Carousel.js';
+import { products } from "../data/data.js";
+
+const renderCarousel = () => {
+  const carouselElement = document.getElementById('carousel-example');
+
+  const items = [
+    {
+      position: 0,
+      el: document.getElementById('carousel-item-1'),
+    },
+    {
+      position: 1,
+      el: document.getElementById('carousel-item-2'),
+    }
+  ];
+
+  // options with default values
+  const options = {
+    defaultPosition: 1,
+    interval: 3000,
+
+    indicators: {
+      activeClasses: 'bg-white dark:bg-gray-800',
+      inactiveClasses:
+        'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
+      items: [
+        {
+          position: 0,
+          el: document.getElementById('carousel-indicator-1'),
+        },
+        {
+          position: 1,
+          el: document.getElementById('carousel-indicator-2'),
+        }
+      ],
+    },
+  };
+
+  // instance options object
+  const instanceOptions = {
+    id: 'carousel-example',
+    override: true
+  };
+
+  // create new instance of Carousel
+  const carousel = new Carousel(carouselElement, items, options, instanceOptions);
+
+  const prevButton = document.getElementById('data-carousel-prev');
+  const nextButton = document.getElementById('data-carousel-next');
+
+  prevButton.addEventListener('click', () => {
+    carousel.prev();
+  });
+
+  nextButton.addEventListener('click', () => {
+    carousel.next();
+  });
+};
+
+const renderMoreProducts = () => {
+  const moreProductsElement = document.getElementById('more-products');
+
+  let indexes = new Set();
+  while (indexes.size < 4) {
+    indexes.add(Math.floor(Math.random() * products.length));
+  }
+
+  const moreProducts = Array.from(indexes).map((index) => {
+    return products[index];
+  });
+
+  moreProductsElement.innerHTML = '';
+  moreProducts.map((product) => {
+    const productElement = document.createElement('div');
+    productElement.classList.add('group', 'relative', 'cursor-pointer');
+    productElement.innerHTML = `
+      <div class="w-full aspect-[1/1] overflow-hidden rounded-md group-hover:opacity-75 p-4 bg-gray-50">
+          <img
+              src="${product.image[0]}"
+              alt="${product.name}"
+              class="object-fit w-full aspect-[1/1] object-contain object-center"
+          />
+      </div>
+      <h3 class="mt-4 text-sm text-gray-700">
+          <a href="./product.html">
+              <span class="absolute inset-0"></span>
+              ${product.name}
+          </a>
+      </h3>
+      <p class="mt-1 text-sm text-gray-500">${product.label}</p>
+      <p class="mt-1 text-sm font-medium text-gray-900">$${product.price}</p>
+    `;
+    moreProductsElement.appendChild(productElement);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderCarousel();
+  renderMoreProducts();
+});
